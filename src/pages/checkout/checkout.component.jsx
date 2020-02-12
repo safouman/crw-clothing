418717ec8fc77be726024696f8a250708c2c3078
Safwen Daghsen <safouman@gmail.com>
2020-02-12 13:33:18 +0100

    moved dir and shop data to its own reducer created respective selectors

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -33,10 +33,7 @@ const CheckoutPage = ({ cartItems, total }) => (
     ))}
 
     <div className="total">
-      <span>
-TOTAL: $
-{total}
-</span>
+      <span>{('TOTAL: $', total)}</span>
     </div>
   </div>
 );
