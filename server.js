d20cbb33417fb2d223d76f0a943276588392d9f1
Safwen Daghsen <safouman@gmail.com>
2020-03-20 15:28:18 +0100

    debugging

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
@@ -26,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log('Server running on port ' + port);
+  console.log('Server is running on port ' + port);
 });
 
 app.get('/service-worker.js', (req, res) => {
