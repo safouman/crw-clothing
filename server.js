6f71c5bea207c6405faf8235b4c3e5f759a94777
Safwen Daghsen <safouman@gmail.com>
2020-03-20 14:47:24 +0100

    debugging

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(compression);
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(compression);
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
