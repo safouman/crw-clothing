d0725a5e1b6e7cbe5cce051004f41826dcfa1e0f
Safwen Daghsen <safouman@gmail.com>
2020-03-20 15:22:18 +0100

    debugging

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const enforce = require('express-sslify');
-const compression = require('compression');
+
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(compression);
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.listen(port, error => {
   if (error) throw error;
-  console.log('Server is running on port ' + port);
+  console.log('Server running on port ' + port);
 });
 
 app.get('/service-worker.js', (req, res) => {
